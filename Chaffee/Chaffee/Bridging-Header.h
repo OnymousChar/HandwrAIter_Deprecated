8d770e9d071a4b9e014b113d7c00e7d19e575388
星野恵瑠 <shonetesmith@gmail.com>
2016-05-21 20:43:45 +0900

    Add WILL SDK

---
+++ Chaffee/Chaffee/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  Chaffee
+//
+//  Created by 史翔新 on 2016/05/21.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "WILLCore/WILLCore.h"
+
+#endif /* Bridging_Header_h */
