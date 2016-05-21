2893fd70c3290237004bd6f16ae6923ce8772551
星野恵瑠 <shonetesmith@gmail.com>
2016-05-22 00:38:27 +0900

    White spaces update

--- Chaffee/Chaffee/ViewController.swift
+++ Chaffee/Chaffee/ViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		let view = DrawView(frame: self.view.bounds)
 		self.view.addSubview(view)
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	
 }
 
